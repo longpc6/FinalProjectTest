366e30456fbb4e355397d9929103c7cb61c1413f
longpc5 <longnuyn392@gmail.com>
2023-12-18 13:15:56 +0700

    Init project

---
+++ NT118-FinalProject/app/src/main/java/com/example/indoorairqualitymonitoringapp/ApiService.java
@@ -0,0 +1,22 @@
+package com.example.indoorairqualitymonitoringapp;
+
+import com.google.gson.JsonObject;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @FormUrlEncoded
+    @POST("/auth/realms/master/protocol/openid-connect/token")
+    Call<JsonObject> getToken(
+            @Field("grant_type") String grantType,
+            @Field("client_id") String clientId,
+            @Field("client_secret") String clientSecret,
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+
+}
